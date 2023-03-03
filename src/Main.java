e13595174d841e1997e8c33d83c2370202b98a5e
Irene Ruiz del portal <irene.ruizdelportal@gmail.com>
2023-03-03 12:57:27 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num1=5;
+        int num2=1;
+        int num3=4;
+
+        int resultados=suma(num1,num2,num3);
+
+        System.out.println("La suma de los 3 es "+resultados);
+
+        }
+
+
+    public static int suma(int a, int b, int c){
+
+        return a+b+c;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
